629b8c65d0299a970730bc6b916a4f658823a40f
May Ng <mngmay@gmail.com>
2019-12-04 15:21:56 -0800

    Added items to rooms.

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,34 @@
 from room import Room
 from player import Player
+from item import Item
+
+# Declare items
+
+item = {'chicken': Item(
+    "Fried Chicken", "Golden and fried to perfection, this piece of poultry is delicious on its own or paired with a waffle."),
+    'shoes': Item("Converse Shoes", "Perfect for protecting your feet and keeping that hipster vibe. Not great for arch support."),
+    'glasses': Item("Black Rimmed Glasses", "Just the thing to view an overlook with.")}
+
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [item['chicken'], item['shoes']]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [item['glasses']]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -35,6 +44,7 @@ room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
 # print("Room after linking: ", room['narrow'].n_to)
+
 #
 # Main
 #
@@ -68,7 +78,7 @@ while game:
     if len(player.current_room.items) > 0:
         print("This room has the following items:")
         for item in player.current_room.items:
-            print(item)
+            print(f"{item.name}: {item.description}")
     else:
         print("This room has no items.")
 
--- src/room.py
+++ src/room.py
@@ -3,11 +3,11 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
-        self.items = []
+        self.items = items
