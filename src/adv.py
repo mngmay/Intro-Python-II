23dedd104ae83c0ca8fa24f65e4838e2d0c8df85
May Ng <mngmay@gmail.com>
2019-12-05 17:00:14 -0800

    refactored how I added items to rooms. now all instantiate with no items and items can be added later as needed.

--- src/adv.py
+++ src/adv.py
@@ -15,21 +15,21 @@ item = {'chicken': Item(
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", []),
+                     "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", [item['chicken'], item['shoes']]),
+passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", [item['glasses']]),
+the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", []),
+to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", [item['wrapper']]),
+earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -45,6 +45,12 @@ room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
 # print("Room after linking: ", room['narrow'].n_to)
+
+# Add items to rooms
+
+room['foyer'].items = [item['chicken'], item['shoes']]
+room['overlook'].items = [item['glasses']]
+room['treasure'].items = [item['wrapper']]
 
 #
 # Main
@@ -103,6 +109,7 @@ while game:
         # grabs current_room of player, looks for direction attribute, defaults to None if invalid.
         new_room = getattr(player.current_room, direction, None)
 
+        # for directional commands n s e w
         if new_room:
             player.current_room = new_room
         elif action == 'get' or action == 'take':
